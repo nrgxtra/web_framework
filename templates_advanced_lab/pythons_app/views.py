8174c601e3c7e871e150f9cc8328b0107dc2cb64
nrgxtra <nrgxtra@hotmail.com>
2022-03-09 12:33:56 +0200

    included upload files

--- templates_advanced_lab/pythons_app/views.py
+++ templates_advanced_lab/pythons_app/views.py
@@ -14,10 +14,9 @@ def create(req):
         form = PythonCreateForm()
         return render(req, 'create.html', {'form': form})
     else:
-        data = req.POST
-        form = PythonCreateForm(data)
+        form = PythonCreateForm(req.POST, req.FILES)
         print(form)
         if form.is_valid():
-            python = form.save()
-            python.save()
+            form.save()
             return redirect('index')
+        return render(req, 'create.html', {'form': form})
